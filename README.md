c388801d19c8e3f4573c7309dac2c1d12bd7d0f3
Katy <ihatetoast@gmail.com>
2020-11-14 17:06:53 -0600

    soft solution to handle dubbul ducimuls and opers

--- README.md
+++ README.md
@@ -6,8 +6,12 @@ A very basic calculator that concats input from key clicks and evaluates using t
 ## Why
 To keep React practice up while working at a place that is all and only jQuery (in 2020!). By using the MathJS libaray and not keeping track of  prev and curr inputs in state, I can focus on just passing props, styling, and — in 2020 — keeping it simple while chaos abounds.
 
-## What I'd do next
-I do not like that 9/0 evaluates to infinity but 9/3-3 evaluates to 0. However, this will lead to another go of making a calculator without MathJS. 
+## What I don't like
+This evaluates a string of inputs. Pressing the keys just concatenates, and .. or */.. will crash it. I chose a simple and not perfect solution to compare the value of the click with the last item in the input string. If neither evaluate as a number using isNan(), then the function just returns. This works for .., .*, *+, etc, but it won't help for situations like "2.3.1".
+
+After much gnashing of teeth, I've decided that this is a simple calc. SIMPLE. I believe to handle the situation above, there'd need to be a set of functions that handle the operations separately, and keeps previous and current entries in separate states.
+
+That, my friends, is another project.
 
 ## Tools:
 * ReactJS / Create React App ... or CRApp
--- src/components/CalculatorBasic.jsx
+++ src/components/CalculatorBasic.jsx
@@ -4,6 +4,7 @@ import ButtonKey from './ButtonKey'
 import ClrButton from './ClrButton'
 import styled from 'styled-components';
 import * as math from 'mathjs';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 const ButtonRow = styled.div`
     display: flex;
@@ -24,27 +25,38 @@ class CalculatorBasic extends Component {
         super(props)
         this.state = {
             input: "", 
-            currentEntry:"",
             isEvaluated: false
         }
 
     }
-    concatInput=(val)=>{
-        this.setState({
-            input: this.state.currentEntry + val,
-        });
 
-        // this.setState({
-        //     input: this.state.input + val,
-        // });
-    
+    addToInput=(val)=>{
+        // does not handle 2.3.4 but this is enough for me in a simple calc
+        const lastClick = this.state.input.slice(-1);
+        if (isNaN(val) && isNaN(lastClick)){
+            return;
+            // this.setState({
+            //     input: "Don't! Now press clear."
+            // });
+            // setTimeout(()=>{
+            //     this.setState({
+            //         input: "0"
+            //     });
+            // }, 2000);
+        }else{
+            this.setState({
+                input: this.state.input + val
+            });
+        }
+        
+        
+
     }
 
     handleEqual=()=>{
         const evalInput = math.evaluate(this.state.input);
         this.setState({
             input:evalInput,
-            currentEntry:"",
             isEvaluated: true
         })
     }
@@ -63,28 +75,28 @@ class CalculatorBasic extends Component {
                 <Screen>{this.state.input}</Screen>
                 <ButtonsWrapper>
                     <ButtonRow>
-                        <ButtonKey val={"8"} face={"8"} handleClick={this.concatInput}/>
-                        <ButtonKey val={"7"} face={"7"} handleClick={this.concatInput}/>
-                        <ButtonKey val={"9"} face={"9"} handleClick={this.concatInput}/>
-                        <ButtonKey val={"/"} face={"/"} handleClick={this.concatInput}/>
+                        <ButtonKey val={"8"} face={"8"} handleClick={this.addToInput}/>
+                        <ButtonKey val={"7"} face={"7"} handleClick={this.addToInput}/>
+                        <ButtonKey val={"9"} face={"9"} handleClick={this.addToInput}/>
+                        <ButtonKey val={"/"} face={"/"} handleClick={this.addToInput}/>
                     </ButtonRow>
                     <ButtonRow>
-                        <ButtonKey val={"5"} face={"5"} handleClick={this.concatInput}/>
-                        <ButtonKey val={"4"} face={"4"} handleClick={this.concatInput}/>
-                        <ButtonKey val={"6"} face={"6"} handleClick={this.concatInput}/>
-                        <ButtonKey val={"*"} face={"x"} handleClick={this.concatInput}/>
+                        <ButtonKey val={"5"} face={"5"} handleClick={this.addToInput}/>
+                        <ButtonKey val={"4"} face={"4"} handleClick={this.addToInput}/>
+                        <ButtonKey val={"6"} face={"6"} handleClick={this.addToInput}/>
+                        <ButtonKey val={"*"} face={"x"} handleClick={this.addToInput}/>
                     </ButtonRow>
                     <ButtonRow>
-                        <ButtonKey val={"1"} face={"1"} handleClick={this.concatInput}/>
-                        <ButtonKey val={"2"} face={"2"} handleClick={this.concatInput}/>
-                        <ButtonKey val={"3"} face={"3"} handleClick={this.concatInput}/>
-                        <ButtonKey val={"-"} face={"-"} handleClick={this.concatInput}/>
+                        <ButtonKey val={"1"} face={"1"} handleClick={this.addToInput}/>
+                        <ButtonKey val={"2"} face={"2"} handleClick={this.addToInput}/>
+                        <ButtonKey val={"3"} face={"3"} handleClick={this.addToInput}/>
+                        <ButtonKey val={"-"} face={"-"} handleClick={this.addToInput}/>
                     </ButtonRow>
                     <ButtonRow>
-                        <ButtonKey val={"."} face={"."} handleClick={this.concatInput}/>
+                        <ButtonKey val={"."} face={"."} handleClick={this.addToInput}/>
                         <ButtonKey val={"0"} face={"0"} handleClick={this.handleZero}/>
                         <ButtonKey val={"="} face={"="} handleClick={this.handleEqual}/>
-                        <ButtonKey val={"+"} face={"+"} handleClick={this.concatInput}/>
+                        <ButtonKey val={"+"} face={"+"} handleClick={this.addToInput}/>
                     </ButtonRow>
                     <div>
                         <ClrButton handleClear={()=>this.setState({input: "", isEvaluated: false})}>Clear</ClrButton>
