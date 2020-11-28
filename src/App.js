eb8bc4c886924da4adc07b082818f33a6a6d89cf
Katy <ihatetoast@gmail.com>
2020-11-28 15:10:44 -0600

    finished.

--- src/App.js
+++ src/App.js
@@ -10,16 +10,18 @@ const genArr = Array.from({length: 25}, (x, i) => i);
 console.log(genArr)
   return (
     <div className="app">
-        <div className="red-line"></div>
-        <Header />
-        <div className="lined-paper-cont" >
-          <ul className="lined-paper">
-            {genArr.map((el,i) => {
-            return <li key={el} className="lines"></li>;
-            })}
-          </ul>
+        <div className="paper">
+          <div className="red-line"></div>
+          <Header />
+          <div className="lined-paper-cont" >
+            <ul className="lined-paper">
+              {genArr.map((el,i) => {
+              return <li key={el} className="lines"></li>;
+              })}
+            </ul>
+          </div>
+          <CalculatorBasic />
         </div>
-        <CalculatorBasic />
       </div>
   );
 }
